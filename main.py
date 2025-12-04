10b5e63beda41106b8f002763a98c51d9cc522d3
Satyawati Tiwari <214369959+Satyawat1@users.noreply.github.com>
2025-12-04 15:21:57 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+def on_on_start():
+    pass
+hourOfAi.on_start(on_on_start)
+
+def on_every():
+    pass
+hourOfAi.every(randint(-180, 180), on_every)
+scene.PHYSICS_PRIORITY
+1 + 1 
---
+++ main.ts
@@ -0,0 +1,8 @@
+hourOfAi.onStart(function on_on_start() {
+    
+})
+hourOfAi.every(randint(-180, 180), function on_every() {
+    
+})
+scene.PHYSICS_PRIORITY
+1 + 1
